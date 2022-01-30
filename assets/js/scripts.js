9fea0c6ef4b5d349825c6982b0cfd227d32e6d95
Sam Mead <samjamead@gmail.com>
2022-01-30 18:36:00 +0100

    Adding detail

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,9 +4,6 @@ jQuery(document).ready(function($){
   var bodyid = $("body").attr('id');
   $("#nav-" + bodyid).addClass('active');
 
-  // Dynamically set bottom margin of body for sticky footer
-  stickyFooter();
-
   // Add target="_blank" to all external links
   $("a[href^='http://']").attr("target","_blank");
   $("a[href^='https://']").attr("target","_blank");
@@ -23,17 +20,3 @@ jQuery(document).ready(function($){
   });
 
 });
-
-// Trigger sizing functions on window resize
-$( window ).resize(function() {
-  stickyFooter();
-});
-
-// Dynamically set bottom margin of body for sticky footer
-function stickyFooter() {
-  setTimeout (function() {
-    var bodyBottomMargin = $("footer").outerHeight();
-    console.log(bodyBottomMargin);
-    $("body").css("margin-bottom", bodyBottomMargin);
-  }, 500);
-}
